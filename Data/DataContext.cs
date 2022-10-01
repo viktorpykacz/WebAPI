8529985a3535a70e1ad06f1fab8027365f861b58
vikto <vikto@VIKTOR_SURFACE>
2022-10-01 23:31:11 +0200

    Dodaj pliki projektów.

---
+++ Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Przychody> Przychody { get; set; }
+        public DbSet<Koszty> Koszty { get; set; }
+
+    }
+}
